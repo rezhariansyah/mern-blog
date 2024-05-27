d9d8b61d509e0789921c121bcf82622f5ea776e4
rezhariansyah <rezharatulangi@gmail.com>
2024-05-28 01:20:18 +0700

    add: flowbite-react UI Component library

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Navbar className="border-b-2">
+      <Link to="/">
+        <span>Dian's</span> Blog
+      </Link>
+    </Navbar>
+  );
+}
